e751d54e587bf64e956680ec3f21d6743c945e3e
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-28 14:46:41 +0530

    :: scope resolution

---
+++ Homework/user Input/scopeResolutionOperator.cpp
@@ -0,0 +1,12 @@
+//  :: - scope resolutionoperator -  In C++ it is used to access members (functions, variables, constants) of a namespace, class, or structure. It specifies the scope from which the member is to be accessed. For example, std::cout accesses the cout object within the std namespace.
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hi Gaurav"; // Hi Gaurav
+
+    return 0;
+}
+
