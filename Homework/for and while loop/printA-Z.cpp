8fa18599b88725a0bc66dd753b2d8a08b355a899
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-29 12:14:09 +0530

    print A-Z

---
+++ Homework/for and while loop/printA-Z.cpp
@@ -0,0 +1,16 @@
+// Print alphabets from A to Z
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    for(ch='A';ch <='Z';ch++) {
+        cout<<ch<<" ";
+    }
+    return 0;
+}
+
+// Output
+// A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 
