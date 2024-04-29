305a945cffd868df2a26b1cf5aef538acaf26c92
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-29 12:36:05 +0530

    Print your name 50 times

---
+++ Homework/for and while loop/03_printName50times.cpp
@@ -0,0 +1,66 @@
+// Print your name 50 times
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cin >> name;
+    for (int i = 1; i <= 50; i++)
+    {
+        cout << i << " " << name << endl;
+    }
+    return 0;
+}
+
+// output
+// 1 Gaurav
+// 2 Gaurav
+// 3 Gaurav
+// 4 Gaurav
+// 5 Gaurav
+// 6 Gaurav
+// 7 Gaurav
+// 8 Gaurav
+// 9 Gaurav
+// 10 Gaurav
+// 11 Gaurav
+// 12 Gaurav
+// 13 Gaurav
+// 14 Gaurav
+// 15 Gaurav
+// 16 Gaurav
+// 17 Gaurav
+// 18 Gaurav
+// 19 Gaurav
+// 20 Gaurav
+// 21 Gaurav
+// 22 Gaurav
+// 23 Gaurav
+// 24 Gaurav
+// 25 Gaurav
+// 26 Gaurav
+// 27 Gaurav
+// 28 Gaurav
+// 29 Gaurav
+// 30 Gaurav
+// 31 Gaurav
+// 32 Gaurav
+// 33 Gaurav
+// 34 Gaurav
+// 35 Gaurav
+// 36 Gaurav
+// 37 Gaurav
+// 38 Gaurav
+// 39 Gaurav
+// 40 Gaurav
+// 41 Gaurav
+// 42 Gaurav
+// 43 Gaurav
+// 44 Gaurav
+// 45 Gaurav
+// 46 Gaurav
+// 47 Gaurav
+// 48 Gaurav
+// 49 Gaurav
+// 50 Gaurav
