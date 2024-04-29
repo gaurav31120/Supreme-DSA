5425528170a2fdda3fe5b89c5129a0b5fec5a142
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-29 12:52:57 +0530

    print a - z

---
+++ Homework/for and while loop/07_printa-z.cpp
@@ -0,0 +1,16 @@
+// Print alphabet a-z
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        cout << ch << " ";
+    }
+}
+
+// output
+// a b c d e f g h i j k l m n o p q r s t u v w x y z
