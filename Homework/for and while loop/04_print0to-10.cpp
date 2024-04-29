deb30bf04a4b4b16d142f908f8bb73f27e03fa35
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-29 12:44:56 +0530

    print 0 to -10

---
+++ Homework/for and while loop/04_print0to-10.cpp
@@ -0,0 +1,30 @@
+// Print from 0 to -10
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    cout << "Print from 0 to -10" << endl;
+
+    while (i >= -10)
+    {
+        cout << i << endl;
+        i--;
+    }
+    return 0;
+}
+// output
+// Print from 0 to -10
+// 0
+// -1
+// -2
+// -3
+// -4
+// -5
+// -6
+// -7
+// -8
+// -9
+// -10
