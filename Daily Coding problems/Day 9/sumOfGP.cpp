cf276d0bda1a9407a21a46fb6cad9827ef7e8dcf
Gaurav Kumar <gaurav31120@gmail.com>
2024-05-11 09:52:46 +0530

    sum of GP using pow function

--- Daily Coding problems/Day 9/sumOfGP.cpp
+++ Daily Coding problems/Day 9/sumOfGP.cpp
@@ -28,3 +28,29 @@ int main()
     cout << "The sum of GP series is: " << sum << endl;
     return 0;
 }
+
+// Approach 2: Using power function.
+
+// #include <bits/stdc++.h>
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     double a; // first num
+//     cout << "Enter first number: " << endl;
+//     cin >> a;
+//     double r; // common ratio
+//     cout << "Enter common ratio: " << endl;
+//     cin >> r;
+//     int n;
+//     cout << "Enter the total numbers will present in the series: " << endl;
+//     cin >> n;
+
+//     double sum = 0;
+
+//     sum = (a * (1 - pow(r, n))) / (1 - r);
+
+//     cout << "The sum of GP series is: " << sum << endl;
+//     return 0;
+// }
